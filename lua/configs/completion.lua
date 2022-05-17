6cd0c48840e810468f278fceeb392e91f8064ab5
mmmeeeker <mmmeeeker@gmail.com>
2022-05-17 04:52:15 -0400

    Some quality of life things + LuaSnips keymaps

--- lua/configs/completion.lua
+++ lua/configs/completion.lua
@@ -64,19 +64,69 @@ cmp.setup({
     documentation = cmp.config.window.bordered(),
   },
   mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), {"i", "c"}),
+    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), {"i", "c"}),
+    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), {"i", "c"}),
+    ['<C-y>'] = cmp.config.disable,
+    ['<C-e>'] = cmp.mapping {
+      i = cmp.mapping.abort(),
+      c = cmp.mapping.close()
+    },
+    ['<CR>'] = cmp.mapping.confirm({ select = false }),
+    ['<TAB>'] = cmp.mapping(
+      function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif luasnip.expandable() then
+          luasnip.expand()
+        elseif luasnip.expand_or_jumpable() then
+          luasnip.expand_or_jump()
+        elseif check_backspace() then
+          fallback()
+        else
+          fallback()
+        end
+      end, {
+        "i",
+        "s",
+      }
+    ),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, {
+      "i",
+      "s",
+    }),
   }),
+    formatting = {
+    fields = { "kind", "abbr", "menu" },
+    format = function(entry, vim_item)
+      -- Kind icons
+      vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+      -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+      vim_item.menu = ({
+        nvim_lsp = "[LSP]",
+        luasnip = "[Snippet]",
+        buffer = "[Buffer]",
+        path = "[Path]",
+      })[entry.source.name]
+      return vim_item
+    end,
+  },
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
-    { name = 'vsnip' },
     { name = 'luasnip' },
   }, {
     { name = 'buffer' },
-  })
+    { name = 'path' }
+  }),
+
 })
 
 cmp.setup.filetype('gitcommit', {
@@ -108,17 +158,18 @@ cmp.setup.cmdline(':', {
 -- Setup lspconfig.
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 local servers = { "asm_lsp",
-  "ocamllsp", 
-  "texlab", 
-  "rust_analyzer", 
-  "clangd", 
-  "pyright", 
-  "julials", 
-  "solang", 
-  "solc", 
-  "cmake", 
-  "zk", 
-  "hls" 
+  "ocamllsp",
+  "texlab",
+  "rust_analyzer",
+  "clangd",
+  "pyright",
+  "julials",
+  "solang",
+  "solc",
+  "cmake",
+  "zk",
+  "hls",
+  "sumneko_lua",
 }
 -- local servers = require("nvim-lsp-installer.servers").get_installed_servers() -- TODO this returns []?
 -- local servers = vim.cmd("LspPrintInstalled")
