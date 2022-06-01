dfdd51ed1205fc994db6a3fe3255c1e1745cd4c7
mmmeeeker <mmmeeeker@gmail.com>
2022-05-31 20:11:09 -0700

    Copilot, Julia, etc

--- lua/configs/completion.lua
+++ lua/configs/completion.lua
@@ -60,8 +60,12 @@ cmp.setup({
     end,
   },
   window = {
-    completion = cmp.config.window.bordered(),
-    documentation = cmp.config.window.bordered(),
+    completion = {
+      cmp.config.window.bordered(),
+    },
+    documentation = {
+      cmp.config.window.bordered(),
+    }
   },
   mapping = cmp.mapping.preset.insert({
     ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), {"i", "c"}),
@@ -118,6 +122,7 @@ cmp.setup({
     end,
   },
   sources = cmp.config.sources({
+    { name = "copilot" },
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
   }, {
