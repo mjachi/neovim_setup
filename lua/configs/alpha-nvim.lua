94a7148638c76150a6e96aac9bd7da0a13a7a217
mjachi <mmmeeeker@gmail.com>
2022-07-20 07:52:32 -0700

    Changed time strings

--- lua/configs/alpha-nvim.lua
+++ lua/configs/alpha-nvim.lua
@@ -40,23 +40,20 @@ local function getGreeting(name)
 	local tableTime = os.date("*t")
 	local hour = tableTime.hour
 	local greetingsTable = {
-		[1] = "  Sleep well",
-		[2] = "  Good morning",
-		[3] = "  Good afternoon",
-		[4] = "  Good evening",
-		[5] = "望 Good night",
+		[1] = "  Good morning",
+		[2] = "  Good afternoon",
+		[3] = "  Good evening",
+		[4] = "望 Good night",
 	}
-	local greetingIndex = ""
-	if hour == 23 or hour < 7 then
+	local greetingIndex = 0
+	if hour < 12 and hour >= 7 then
 		greetingIndex = 1
-	elseif hour < 12 then
-		greetingIndex = 2
 	elseif hour >= 12 and hour < 18 then
-		greetingIndex = 3
+		greetingIndex = 2
 	elseif hour >= 18 and hour < 21 then
+		greetingIndex = 3
+	else
 		greetingIndex = 4
-	elseif hour >= 21 then
-		greetingIndex = 5
 	end
 	return greetingsTable[greetingIndex] .. ", " .. name
 end
